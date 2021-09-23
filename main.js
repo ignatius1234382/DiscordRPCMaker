5530180985f1013dae89ad797257274d08f772ef
ignatius1234382 <91234171+ignatius1234382@users.noreply.github.com>
2021-09-23 10:11:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ if (id !== undefined) {
 	if (options.largeimage !== '') {
 		activity.largeImageKey = options.largeimage
 		// If you change this and some asks about this, please still give me credit :)
-		activity.largeImageText = "Made with ThatOneCalculator's Discord RPC Maker (v2.1.1 CLI)!"
+		activity.largeImageText = "Discord Default Status"
 	}
 	if (options.smallimage !== '') {
 		activity.smallImageKey = options.smallimage
